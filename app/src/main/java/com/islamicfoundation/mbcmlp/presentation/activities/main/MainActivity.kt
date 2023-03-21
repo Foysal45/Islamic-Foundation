d457b2e272e518072d501a956a29e2bcd3eca859
Md.Foysal Ahmed <91059464+Foysal45@users.noreply.github.com>
2023-03-21 09:34:50 +0600

    dashboard

--- app/src/main/java/com/islamicfoundation/mbcmlp/presentation/activities/main/MainActivity.kt
+++ app/src/main/java/com/islamicfoundation/mbcmlp/presentation/activities/main/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.islamicfoundation.mbcmlp.presentation.activities.main
 
+import DrawerListItem
 import android.os.Bundle
-import android.view.Menu
+import android.view.MenuItem
+import android.widget.ExpandableListView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.*
 import com.google.android.material.navigation.NavigationView
 import com.islamicfoundation.mbcmlp.R
+import com.islamicfoundation.mbcmlp.activity.toast
 import com.islamicfoundation.mbcmlp.databinding.ActivityMainBinding
 import com.islamicfoundation.mbcmlp.presentation.activities.base.BaseActivity
-import com.islamicfoundation.mbcmlp.presentation.fragment.auth.LoginFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -20,6 +22,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override fun initializeViewBinding() = ActivityMainBinding.inflate(layoutInflater)
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private var navigationMenuId: Int = 0
+    private var menuItem: MenuItem? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,23 +33,44 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
 
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Set up expandable ListView
+       // val listView = findViewById<ExpandableListView>(R.id.nav_expandable_list)
+        val drawerItems = listOf(
+            DrawerListItem(listOf("Item 1", "Item 2", "Item 3")),
+            //DrawerListItem(listOf("Item 4", "Item 5", "Item 6")),
+           // DrawerListItem(listOf("Item 7", "Item 8", "Item 9"))
+        )
+
+     navView.setNavigationItemSelectedListener { item ->
+         navigationMenuId = item.itemId
+         menuItem = item
+         when (menuItem!!.itemId) {
+            R.id.nav_visit -> {
+                Toast.makeText(this, "clicked",Toast.LENGTH_SHORT ).show()
+                val adapter = ExpandableListAdapter(this, drawerItems)
+                //listView.setAdapter(adapter)
+             }
+         }
+         true
+        }
+
     }
+
+
 
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
-
-
-
 
 }
