fc33c3f3581b09813430429f8b1a225f47cb49ab
Md.Foysal Ahmed <91059464+Foysal45@users.noreply.github.com>
2023-03-22 18:57:17 +0600

    form design ongoing

--- app/src/main/java/com/islamicfoundation/mbcmlp/presentation/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/islamicfoundation/mbcmlp/presentation/fragment/home/HomeFragment.kt
@@ -39,13 +39,14 @@ class HomeFragment : Fragment() {
 
     private fun barChartForLastSevenDaysVisitEducationCentre(){
 
-        binding?.barChartLastSevenDaysVisitEducationCentre?.setDrawBarShadow(false)
+        binding?.barChartLastSevenDaysVisitEducationCentre?.setDrawBarShadow(true)
         binding?.barChartLastSevenDaysVisitEducationCentre?.setDrawValueAboveBar(true)
         binding?.barChartLastSevenDaysVisitEducationCentre?.setMaxVisibleValueCount(50)
         binding?.barChartLastSevenDaysVisitEducationCentre?.setPinchZoom(false)
-        binding?.barChartLastSevenDaysVisitEducationCentre?.setDrawGridBackground(true)
+        binding?.barChartLastSevenDaysVisitEducationCentre?.setDrawGridBackground(false)
         binding?.barChartLastSevenDaysVisitEducationCentre?.description?.isEnabled = false
         binding?.barChartLastSevenDaysVisitEducationCentre?.animateY(2000)
+        binding?.barChartLastSevenDaysVisitEducationCentre?.notifyDataSetChanged()
 
         val xAxis = binding?.barChartLastSevenDaysVisitEducationCentre?.xAxis
         xAxis?.position = XAxis.XAxisPosition.BOTTOM
@@ -54,27 +55,30 @@ class HomeFragment : Fragment() {
         xAxis?.labelCount = 5
 
         val leftAxis = binding?.barChartLastSevenDaysVisitEducationCentre?.axisLeft
-        leftAxis?.setDrawGridLines(true)
-        leftAxis?.spaceTop = 30f
-        leftAxis?.axisMinimum = 0f
+        leftAxis?.setDrawGridLines(false)
+        leftAxis?.setDrawGridLines(false)
+        leftAxis?.isEnabled = false
 
         val rightAxis = binding?.barChartLastSevenDaysVisitEducationCentre?.axisRight
         rightAxis?.isEnabled = false
 
         val entries: MutableList<BarEntry> = ArrayList()
-        entries.add(BarEntry(1f, 1f))
-        entries.add(BarEntry(2f, 2f))
-        entries.add(BarEntry(3f, 3f))
+        entries.add(BarEntry(1f, 9f))
+        entries.add(BarEntry(2f, 7f))
+        entries.add(BarEntry(3f, 8f))
         entries.add(BarEntry(4f, 4f))
         entries.add(BarEntry(5f, 5f))
         entries.add(BarEntry(6f, 6f))
         entries.add(BarEntry(7f, 7f))
 
-        val dataSet = BarDataSet(entries, "গত সাত দিনে পরিদর্শিত শিক্ষা কেন্দ্র")
+
+
+        val dataSet = BarDataSet(entries, "My Bar Chart")
         dataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
         dataSet.setDrawValues(true)
         dataSet.valueTextColor = Color.DKGRAY
-        dataSet.valueTextSize = 10f
+        dataSet.valueTextSize = 12f
+
 
         val data = BarData(dataSet)
         binding?.barChartLastSevenDaysVisitEducationCentre?.data = data
@@ -88,7 +92,7 @@ class HomeFragment : Fragment() {
         binding?.barChartLastSevenDaysVisitResourceCentre?.setDrawValueAboveBar(true)
         binding?.barChartLastSevenDaysVisitResourceCentre?.setMaxVisibleValueCount(50)
         binding?.barChartLastSevenDaysVisitResourceCentre?.setPinchZoom(false)
-        binding?.barChartLastSevenDaysVisitResourceCentre?.setDrawGridBackground(true)
+        binding?.barChartLastSevenDaysVisitResourceCentre?.setDrawGridBackground(false)
         binding?.barChartLastSevenDaysVisitResourceCentre?.description?.isEnabled = false
         binding?.barChartLastSevenDaysVisitResourceCentre?.animateY(2000)
         binding?.barChartLastSevenDaysVisitResourceCentre?.notifyDataSetChanged()
@@ -103,23 +107,14 @@ class HomeFragment : Fragment() {
         leftAxis?.setDrawGridLines(false)
         leftAxis?.setDrawGridLines(false)
         leftAxis?.isEnabled = false
-        //leftAxis?.spaceTop = 0f
-        //leftAxis?.axisMinimum = 0f
 
-      /*  leftAxis?.valueFormatter = object : ValueFormatter() {
-            override fun getFormattedValue(value: Float): String {
-                // You can return any text you want here based on the value
-                return "okey"
-            }
-        }
-*/
         val rightAxis = binding?.barChartLastSevenDaysVisitResourceCentre?.axisRight
         rightAxis?.isEnabled = false
 
         val entries: MutableList<BarEntry> = ArrayList()
-        entries.add(BarEntry(1f, 1f))
-        entries.add(BarEntry(2f, 2f))
-        entries.add(BarEntry(3f, 3f))
+        entries.add(BarEntry(1f, 9f))
+        entries.add(BarEntry(2f, 7f))
+        entries.add(BarEntry(3f, 8f))
         entries.add(BarEntry(4f, 4f))
         entries.add(BarEntry(5f, 5f))
         entries.add(BarEntry(6f, 6f))
